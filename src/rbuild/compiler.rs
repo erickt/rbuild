f65ed5df8f4fb380cf838cce955613382de2e119
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2014-03-05 21:27:31 -0800

    Clean up code

--- src/rbuild/compiler.rs
+++ src/rbuild/compiler.rs
@@ -1,5 +1,4 @@
 use std::io::process::Process;
-use std::str;
 
 use context::{Context, Call};
 
@@ -21,136 +20,138 @@ impl Compiler {
 
     pub fn compile(&self, src: Path) -> Compile {
         Compile {
-            ctx: self.ctx.clone(),
-            exe: self.exe.clone(),
-            dst: None,
-            srcs: ~[src],
-            flags: ~[~"-c"],
+            gcc: Gcc {
+                ctx: self.ctx.clone(),
+                exe: self.exe.clone(),
+                dst: None,
+                srcs: ~[src],
+                flags: ~[~"-c"],
+            }
         }
     }
 
     pub fn link_exe(&self, dst: Path, srcs: ~[Path]) -> LinkExe {
         LinkExe {
-            ctx: self.ctx.clone(),
-            exe: self.exe.clone(),
-            dst: dst,
-            srcs: srcs,
-            flags: ~[],
-        }
-    }
-
-    /*
-    pub fn build_object_with(&self, dst: Option<Path>, src: Path, flags: &[~str]) -> Path {
-        let dst = dst.or_else(|| {
-            Some(src.clone().with_extension("o"))
-        });
-
-        let call = Compile {
-            exe: self.exe.clone(),
-            dst: dst,
-            srcs: ~[src],
-            flags: vec::append(self.flags.clone(), flags),
-        };
-
-        let mut prep = self.ctx.prep("GccCall");
-
-        prep.declare_path("exe", &call.exe);
-        prep.declare_path("dst", &dst);
-        prep.declare_paths("srcs", call.srcs);
-
-        let cached_call = ~[
-            context::Path(self.exe.clone()),
-        ];
-
-        match dst {
-            Some(ref dst) => {
-                cached_call.push(context::Str(~"-o"));
-                cached_call.push(context::Path(dst));
+            gcc: Gcc {
+                ctx: self.ctx.clone(),
+                exe: self.exe.clone(),
+                dst: Some(dst),
+                srcs: srcs,
+                flags: ~[],
             }
-            None => { }
-        }
-
-        for flag in self.flags.iter().zip(flags.iter()) {
-            cached_call.push(context::Str(flag.to_owned()));
-        }
-
-
-        prep.exec(proc(exec) {
-            call.run().unwrap();
-            let dst = call.dst.unwrap();
-            dst
-        })
-    }
-    */
-
-    /*
-    pub fn build_objects(&self, srcs: &[Path]) -> ~[Call] {
-        srcs.iter().map(|src| {
-            self.build_object(src.clone())
-        }).collect()
-    }
-
-    pub fn build_exe(&self, dst: Path, srcs: ~[Path]) -> Call {
-        let call = Call {
-            exe: self.exe.clone(),
-            dst: Some(dst),
-            srcs: srcs,
-            flags: self.flags.clone(),
-        };
-
-        Call {
-            ctx: self.ctx.clone(),
-            gcc: gcc,
         }
     }
-    */
 }
 
 pub struct Compile {
-    priv ctx: Context,
-    priv exe: Path,
-    priv dst: Option<Path>,
-    priv srcs: ~[Path],
-    priv flags: ~[~str],
+    priv gcc: Gcc,
 }
 
 impl Compile {
-    pub fn set_dst(mut self, dst: Path) -> Compile {
+    pub fn set_dst(self, dst: Path) -> Compile {
+        let Compile { gcc } = self;
+        Compile { gcc: gcc.set_dst(dst) }
+    }
+
+    pub fn add_src(self, src: Path) -> Compile {
+        let Compile { gcc } = self;
+        Compile { gcc: gcc.add_src(src) }
+    }
+
+    pub fn add_flag<S: Str>(self, flag: S) -> Compile {
+        let Compile { gcc } = self;
+        Compile { gcc: gcc.add_flag(flag) }
+    }
+
+    pub fn run(self) -> Path {
+        let Compile { mut gcc } = self;
+
+        assert!(!gcc.srcs.is_empty());
+
+        let dst = match gcc.dst {
+            Some(ref dst) => dst.clone(),
+            None => {
+                let dst = gcc.srcs[0].with_extension("o");
+                gcc.dst = Some(dst.clone());
+                dst
+            }
+        };
+
+        gcc.run();
+
+        dst
+    }
+}
+
+pub struct LinkExe {
+    priv gcc: Gcc,
+}
+
+impl LinkExe {
+    pub fn add_src(self, src: Path) -> LinkExe {
+        let LinkExe { gcc } = self;
+        LinkExe { gcc: gcc.add_src(src) }
+    }
+
+    pub fn add_flag<S: Str>(self, flag: S) -> LinkExe {
+        let LinkExe { gcc } = self;
+        LinkExe { gcc: gcc.add_flag(flag) }
+    }
+
+    pub fn run(self) -> Path {
+        let LinkExe { gcc } = self;
+
+        assert!(!gcc.srcs.is_empty());
+
+        let dst = match gcc.dst {
+            Some(ref dst) => dst.clone(),
+            None => { fail!("expected dst") }
+        };
+
+        gcc.run();
+
+        dst
+    }
+}
+
+struct Gcc {
+    ctx: Context,
+    exe: Path,
+    dst: Option<Path>,
+    srcs: ~[Path],
+    flags: ~[~str],
+}
+
+impl Gcc {
+    pub fn set_dst(mut self, dst: Path) -> Gcc {
         self.dst = Some(dst);
         self
     }
 
-    pub fn add_src(mut self, src: Path) -> Compile {
+    pub fn add_src(mut self, src: Path) -> Gcc {
         self.srcs.push(src);
         self
     }
 
-    pub fn add_flag<S: Str>(mut self, flag: S) -> Compile {
+    pub fn add_flag<S: Str>(mut self, flag: S) -> Gcc {
         self.flags.push(flag.as_slice().to_owned());
         self
     }
 
-    pub fn add_flags<S: Str>(mut self, flags: &[S]) -> Compile {
-        for flag in flags.iter() {
-            let flag = flag.as_slice().to_owned();
-            self.flags.push(flag);
-        }
+    fn run(self) {
+        let Gcc { ctx, exe, dst, srcs, flags } = self;
 
-        self
-    }
-
-    pub fn run(self) -> Path {
-        let Compile { ctx, exe, dst, srcs, flags } = self;
-
-        let dst = match dst {
-            Some(dst) => dst,
-            None => srcs.get(0).unwrap().with_extension("o"),
-        };
+        assert!(!srcs.is_empty());
 
         let mut call = Call::new(exe.clone());
 
-        call.push_str(~"-o");
-        call.push_output_path(dst.clone());
+        match dst {
+            Some(ref dst) => {
+                call.push_str(~"-o");
+                call.push_output_path(dst.clone());
+            }
+            None => { }
+        }
 
         for flag in flags.move_iter() {
             call.push_str(flag);
@@ -172,7 +173,11 @@ impl Compile {
                 print!(" {}", src.display());
             }
 
-            println!(" -> {}", dst.display());
+            match dst {
+                Some(ref dst) => { println!(" -> {}", dst.display()); }
+                None => { println!(""); }
+            }
+
             println!("{} {}", prog, args);
 
             let mut process = Process::new(prog, args).unwrap();
@@ -181,282 +186,6 @@ impl Compile {
             if !status.success() {
                 fail!("command failed");
             }
-
-            dst
-        })
-
-
-
-        /*
-
-        let exe = self.exe.as_str().unwrap().to_owned();
-
-        let mut cmd = ~[];
-
-        match self.dst {
-            Some(ref dst) => {
-                cmd.push_all([~"-o", dst.as_str().unwrap().to_owned()]);
-            }
-            None => {}
-        }
-
-        for flag in self.flags.iter() {
-            cmd.push(flag.as_slice().to_owned());
-        }
-
-        for src in self.srcs.iter() {
-            cmd.push(src.as_str().unwrap().to_owned());
-        }
-
-        print!("{}:", self.exe.display());
-        for src in self.srcs.iter() {
-            print!(" {}", src.display());
-        }
-
-        match self.dst {
-            Some(ref dst) => {
-                println!(" -> {}", dst.display());
-            }
-            None => {
-                println!("");
-            }
-        }
-
-        let status = try!(Process::status(exe, cmd));
-
-        if !status.success() {
-            fail!("command failed: {:?}", cmd);
-        }
-
-        Ok(())
-        */
-    }
-}
-
-pub struct LinkExe {
-    priv ctx: Context,
-    priv exe: Path,
-    priv dst: Path,
-    priv srcs: ~[Path],
-    priv flags: ~[~str],
-}
-
-impl LinkExe {
-    pub fn add_src(mut self, src: Path) -> LinkExe {
-        self.srcs.push(src);
-        self
-    }
-
-    pub fn add_flag<S: Str>(mut self, flag: S) -> LinkExe {
-        self.flags.push(flag.as_slice().to_owned());
-        self
-    }
-
-    pub fn add_flags<S: Str>(mut self, flags: &[S]) -> LinkExe {
-        for flag in flags.iter() {
-            let flag = flag.as_slice().to_owned();
-            self.flags.push(flag);
-        }
-
-        self
-    }
-
-    pub fn run(self) -> Path {
-        let LinkExe { ctx, exe, dst, srcs, flags } = self;
-
-        let mut call = Call::new(exe.clone());
-
-        call.push_str(~"-o");
-        call.push_output_path(dst.clone());
-
-        for flag in flags.move_iter() {
-            call.push_str(flag);
-        }
-
-        for src in srcs.iter() {
-            call.push_input_path(src.clone());
-        }
-
-        let mut prep = ctx.prep("Call");
-        prep.declare_call(&call);
-
-        prep.exec(proc(_exec) {
-            let (prog, args) = call.cmd();
-
-            print!("{}:", exe.display());
-
-            for src in srcs.iter() {
-                print!(" {}", src.display());
-            }
-
-            println!(" -> {}", dst.display());
-            println!("{} {}", prog, args);
-
-            let mut process = Process::new(prog, args).unwrap();
-            let status = process.wait_with_output();
-
-            if !status.status.success() {
-                let out = str::from_utf8(status.output).unwrap();
-                let err = str::from_utf8(status.error).unwrap();
-                println!("{}{}", out, err);
-                fail!("command failed");
-            }
-
-            dst
         })
     }
 }
-
-/*
-pub struct Call {
-    priv ctx: Context,
-    priv gcc: Gcc,
-}
-
-impl Call {
-    pub fn add_flags<S: Str>(&mut self, flags: &[S]) {
-        self.gcc.add_flags(flags)
-    }
-
-    pub fn run(self) -> Path {
-        let Call { ctx, gcc } = self;
-
-        let mut prep = ctx.prep("Call");
-
-        prep.declare_input("value", "gcc", &gcc);
-        prep.declare_path("exe", &gcc.exe);
-        prep.declare_paths("srcs", gcc.srcs);
-
-        prep.exec(proc(exec) {
-            gcc.run().unwrap();
-            let dst = gcc.dst.unwrap();
-            exec.discover_output_path("dst", &dst);
-            dst
-        })
-    }
-}
-*/
-
-/*
-struct BuildObjectsExec {
-    ctx: Context,
-    opts: BuildObjectOptions,
-}
-
-struct BuildObjectsOptions {
-    exe: Path,
-    srcs: ~[Path],
-    flags: ~[~str],
-}
-
-impl Call {
-    pub fn run(self) -> Path {
-        let Call { ctx, opts } = self;
-
-        let mut prep = ctx.prep("BuildObjectExec");
-
-        prep.declare_path(&opts.exe);
-
-        for src in opts.srcs {
-            prep.declare_path(&src);
-        }
-
-        prep.declare_input("flags", "flags", &opts.flags);
-
-        let (port,chan) = comm::stream();
-        chan.send(opts);
-
-        do prep.exec |exec| {
-            let GccOptions { exe, dst, src, flags } = port.recv();
-
-            let mut cmd = ~[exe.to_str()];
-
-            let dst = match dst {
-                Some(dst) => dst,
-                None => Path(src.to_str() + ".o"),
-            };
-
-            cmd.push_all([~"-o", dst.to_str()]);
-
-            for flag in flags.move_iter() {
-                cmd.push(flag);
-            }
-
-            cmd.push(src.to_str());
-
-            println!("{}: {} -> {}", exe.to_str(), src.to_str(), dst.to_str());
-
-            let status = run::process_status(*cmd.head(), cmd.tail());
-
-            if status == 0 {
-                exec.declare_output_path(&dst);
-                dst
-            } else {
-                fail!("command failed: %?", cmd);
-            }
-        }
-    }
-}
-
-struct BuildExeExec {
-    ctx: Context,
-    opts: BuildExeOptions,
-}
-
-struct BuildExeOptions {
-    exe: Path,
-    dst: Path,
-    srcs: ~[Path],
-    flags: ~[~str],
-}
-
-impl BuildExeExec {
-    pub fn run(self) -> ~str {
-        let BuildExeExec { ctx, opts } = self;
-
-        let mut prep = ctx.prep("BuildExeExec");
-
-        prep.declare_path(&opts.exe);
-        prep.declare_path(&opts.dst);
-
-        for src in opts.srcs.iter() {
-            prep.declare_path(src);
-        }
-
-        prep.declare_input("flags", "flags", &opts.flags);
-
-        let (port,chan) = comm::stream();
-        chan.send(opts);
-
-        do prep.exec |exec| {
-            let BuildExeOptions { exe, dst, srcs, flags } = port.recv();
-
-            let mut cmd = ~[exe.to_str()];
-
-            cmd.push_all([~"-o", dst.to_str()]);
-
-            for flag in flags.move_iter() {
-                cmd.push(flag);
-            }
-
-            for src in srcs.iter() {
-                cmd.push(src.to_str());
-            }
-
-            println!("{}: {} -> {}",
-                exe.to_str(),
-                srcs.map(|src| src.to_str()).connect(" "),
-                dst.to_str());
-
-            let status = run::process_status(*cmd.head(), cmd.tail());
-
-            if status == 0 {
-                exec.declare_output_path(&dst);
-                dst.to_str()
-            } else {
-                fail!("command failed: %?", cmd);
-            }
-        }
-    }
-}
-*/
--- src/rbuild/context.rs
+++ src/rbuild/context.rs
@@ -21,12 +21,7 @@ impl Context {
         let cfg = TreeMap::new();
 
         let mut freshness = TreeMap::new();
-        //freshness.insert(~"path", path_is_fresh);
         freshness.insert(~"call", call_is_fresh);
-        /*
-        freshness.insert(~"paths", paths_are_fresh);
-        freshness.insert(~"value", value_is_fresh);
-        */
 
         let ctx = workcache::Context::new_with_freshness(db, logger, cfg, freshness);
 
@@ -35,6 +30,12 @@ impl Context {
 
     pub fn prep<'a>(&'a self, fn_name: &'a str) -> Prep<'a> {
         Prep { prep: self.ctx.prep(fn_name) }
+    }
+
+    pub fn prep_call<'a>(&'a self, fn_name: &'a str, call: &Call) -> Prep<'a> {
+        let mut prep = self.prep(fn_name);
+        prep.declare_call(call);
+        prep
     }
 }
 
@@ -52,17 +53,6 @@ impl<'a> Prep<'a> {
     pub fn declare_call(&mut self, call: &Call) {
         self.declare_input("call", "", call)
     }
-    /*
-    pub fn declare_path(&mut self, name: &str, path: Path) {
-        self.declare_input("path", name, &CachedPath::new(path))
-    }
-
-    pub fn declare_paths(&mut self, name: &str, paths: &[Path]) {
-        for path in paths.iter() {
-            self.declare_path(name, path)
-        }
-    }
-    */
 
     pub fn exec<
         'a,
@@ -87,16 +77,8 @@ impl<'a> Exec<'a> {
     }
 
     pub fn discover_input_path(&mut self, name: &str, path: Path) {
-        self.discover_input("path", name, &CachedPath::new(path))
+        self.discover_input("path", name, &InputPath::new(path))
     }
-
-    /*
-    pub fn discover_input_paths(&mut self, name: &str, paths: &[Path]) {
-        for path in paths.iter() {
-            self.discover_input_path(name, path)
-        }
-    }
-    */
 
     pub fn discover_output<
         T: Encodable<json::Encoder<'a>>
@@ -105,20 +87,12 @@ impl<'a> Exec<'a> {
     }
 
     pub fn discover_output_path(&mut self, name: &str, path: Path) {
-        self.discover_output("path", name, &CachedPath::new(path))
+        self.discover_output("path", name, &path)
     }
-
-    /*
-    pub fn discover_output_paths(&mut self, name: &str, paths: &[Path]) {
-        for path in paths.iter() {
-            self.discover_output_path(name, path)
-        }
-    }
-    */
 }
 
 /// Hashes the path contents
-pub fn digest_path(path: &Path) -> IoResult<~str> {
+fn digest_path(path: &Path) -> IoResult<~str> {
     let mut file = try!(File::open(path));
     let bytes = try!(file.read_to_end());
     let digest = hash::hash(&bytes);
@@ -128,27 +102,19 @@ pub fn digest_path(path: &Path) -> IoResult<~str> {
     Ok(digest.to_str_radix(16))
 }
 
-/// Hashes only the last-modified time
-pub fn digest_only_date(path: &Path) -> IoResult<~str> {
-    let st = try!(path.stat());
-    let digest = hash::hash(&st.modified);
-
-    Ok(digest.to_str_radix(16))
-}
-
 #[deriving(Encodable, Decodable)]
-struct CachedPath {
+struct InputPath {
     path: Path,
     digest: ~str,
     modified: u64,
 }
 
-impl CachedPath {
-    fn new(path: Path) -> CachedPath {
+impl InputPath {
+    fn new(path: Path) -> InputPath {
         let digest = digest_path(&path).unwrap();
         let st = path.stat().unwrap();
 
-        CachedPath {
+        InputPath {
             path: path,
             digest: digest,
             modified: st.modified,
@@ -173,7 +139,7 @@ pub struct Call {
 impl Call {
     pub fn new(prog: Path) -> Call {
         Call {
-            prog: InputPath(CachedPath::new(prog)),
+            prog: InputPath(InputPath::new(prog)),
             args: ~[],
         }
     }
@@ -183,7 +149,7 @@ impl Call {
     }
 
     pub fn push_input_path(&mut self, value: Path) {
-        self.args.push(InputPath(CachedPath::new(value)))
+        self.args.push(InputPath(InputPath::new(value)))
     }
 
     pub fn push_output_path(&mut self, value: Path) {
@@ -213,7 +179,7 @@ impl Call {
 #[deriving(Encodable, Decodable)]
 enum CallArg {
     Str(~str),
-    InputPath(CachedPath),
+    InputPath(InputPath),
     OutputPath(Path),
 }
 
@@ -226,30 +192,6 @@ impl CallArg {
         }
     }
 }
-
-/*
-trait ToCallArg {
-    fn into_call_arg(self) -> CallArg;
-}
-
-impl<'a> ToCallArg for &'a str {
-    fn into_call_arg(self) -> CallArg {
-        Str(self.to_owned())
-    }
-}
-
-impl ToCallArg for ~str {
-    fn into_call_arg(self) -> CallArg {
-        Str(self)
-    }
-}
-
-impl ToCallArg for Path {
-    fn into_call_arg(self) -> CallArg {
-        Path(CachedPath::new(self))
-    }
-}
-*/
 
 fn json_encode<'a, T:Encodable<json::Encoder<'a>>>(t: &T) -> ~str {
     let mut writer = MemWriter::new();
@@ -264,35 +206,8 @@ fn json_decode<T:Decodable<json::Decoder>>(s: &str) -> T {
     Decodable::decode(&mut decoder)
 }
 
-/*
-fn path_is_fresh(_name: &str, value: &str) -> bool {
-    let path: CachedPath = json_decode(value);
-
-    path.is_fresh()
-}
-*/
-
 fn call_is_fresh(_name: &str, value: &str) -> bool {
     let call: Call = json_decode(value);
 
     call.is_fresh()
 }
-
-/*
-extern "C" fn paths_are_fresh(path: &str, in_hash: u64) -> bool {
-    let path = Path::new(path);
-
-    println!("paths_are_fresh: {} {}", path.display(), in_hash);
-
-    path.exists() && in_hash == digest_path(&path).unwrap()
-}
-
-
-extern "C" fn value_is_fresh(path: &str, in_hash: u64) -> bool {
-    let path = Path::new(path);
-
-    println!("value_is_fresh: {} {}", path.display(), in_hash);
-
-    in_hash == digest_path(&path).unwrap()
-}
-*/
